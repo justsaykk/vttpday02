c4342fd2909e530f7b7e6af9431685caa8b9dc4a
justsaykk <kaikein.woo@gmail.com>
2022-05-31 11:30:06 +0800

    optimized SumAll

--- myapp/core/SumAll.java
+++ myapp/core/SumAll.java
@@ -5,18 +5,13 @@ package myapp.core;
 public class SumAll {
     public static void main(String[] args) {
 
-        // Convert elements from String to Int
-        int[] newArray = new int[args.length];
+        // Initialize new variable
         int sum = 0;
 
         for (int i = 0; i < args.length; i++) {
-            newArray[i] = Integer.parseInt(args[i]);
-
+            sum += Integer.parseInt(args[i]);
         }
 
-        for (int i = 0; i < newArray.length; i++) {
-            sum += newArray[i];
-        }
         System.out.printf("The sum is %d", sum);
 
     }
